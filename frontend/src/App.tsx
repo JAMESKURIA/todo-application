9cb7b2b51e83b31d313b05c118c712e7b5981b7b
JAMES KURIA CHEGE <jameskuria04@gmail.com>
2024-04-20 11:19:08 +0300

    Create todo frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { FormEvent } from "react";
+
+const STATUS = {
+    TODO: "Todo",
+    IN_PROGRESS: "In Progress",
+    DONE: "Done",
+};
+
+type Task = {
+    id?: string;
+    todoItem: string;
+    status: keyof typeof STATUS;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [tasks, setTasks] = React.useState<Task[]>([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function addTodo(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const todoItem = formData.get("todoItem");
+        const status = formData.get("status");
+
+        const newTodo = {
+            id: Date.now().toString(),
+            todoItem,
+            status,
+        } as Task;
+
+        setTasks((prev) => [...prev, newTodo]);
+    }
+
+    function removeTodo(id: string) {
+        setTasks((prev) => prev.filter((task) => task.id !== id));
+    }
+
+    return (
+        <>
+            <form onSubmit={addTodo}>
+                <input name="todoItem" type="text" />
+                <select name="status">
+                    <option value="TODO">Todo</option>
+                    <option value="IN_PROGRESS">In Progress</option>
+                    <option value="DONE">Done</option>
+                </select>
+                <button type="submit">Add</button>
+            </form>
+
+            <ul>
+                {tasks.map((task, idx) => (
+                    <li key={idx.toString()}>
+                        <span>{task.todoItem}</span>
+                        <span>{task.todoItem}</span>
+                        <span>
+                            <button
+                                type="button"
+                                onClick={() => removeTodo(task.id!)}
+                            >
+                                Delete
+                            </button>
+                        </span>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
-export default App
+export default App;
