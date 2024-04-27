38a238b1b4897a86b1cb8017bda7c7240163e048
JAMES KURIA CHEGE <jameskuria04@gmail.com>
2024-04-27 12:20:06 +0300

    Integrate the backend to the frontend

--- backend/app.js
+++ backend/app.js
@@ -1,52 +1,42 @@
-const express  = require("express")
+const express = require("express");
 
-const app  = express()
-const port  = 8081
+const app = express();
+const port = 8081;
 
-
-app.use(express.json())
+app.use(express.json());
+app.use(require("cors")());
 
 let tasks = [
     {
-        id:1,
-        taskName:"Laundry",
-        status:"TODO",
+        id: 1,
+        taskName: "Laundry",
+        status: "TODO",
+    },
+    {
+        id: 2,
+        taskName: "Cleaning",
+        status: "DONE",
+    },
+];
 
-    }, {
-        id:2,
-        taskName:"Cleaning",
-        status:"DONE",
-
-    }
-
-]
-
-app.get("/hello",(req,res)=>{
-    res.send("Hi Dev")
-})
+app.get("/hello", (req, res) => {
+    res.send("Hi Dev");
+});
 
 // Getting tasks
-app.get("/task",(req,res)=>{
-
-    res.json(tasks)
-
-})
+app.get("/task", (req, res) => {
+    res.json(tasks);
+});
 
 // adding a task
-app.post("/task",(req,res)=>{
-    newTask= req.body
-    tasks.push(newTask)
-    res.send("Task Added Successfully")
-
-
-})
-
-
-
+app.post("/task", (req, res) => {
+    newTask = req.body;
+    tasks.push({ ...newTask, id: Date.now().toString() });
+    res.send("Task Added Successfully");
+});
 
 // GET,POST , DELETE , PUT
 
-
-app.listen(port,(err)=>{
-    console.log("app is running on port :" + port)
+app.listen(port, (err) => {
+    console.log("app is running on port :" + port);
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import React, { FormEvent } from "react";
 import axios from "axios";
+import React, { FormEvent } from "react";
 
 const STATUS = {
     TODO: "Todo",
@@ -11,10 +11,18 @@ type Task = {
     id?: string;
     todoItem: string;
     status: keyof typeof STATUS;
+    todoDescription: string;
 };
 
 function App() {
     const [tasks, setTasks] = React.useState<Task[]>([]);
+
+    const [uploadStatus, setUploadStatus] = React.useState<
+        "loading" | "success" | "error" | undefined
+    >();
+    const [fetchTaskStatus, setFetchTaskStatus] = React.useState<
+        "loading" | "success" | "error" | undefined
+    >();
 
     function addTodo(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
@@ -22,26 +30,94 @@ function App() {
         const formData = new FormData(e.currentTarget);
         const todoItem = formData.get("todoItem");
         const status = formData.get("status");
+        const todoDescription = formData.get("todoDescription");
 
         const newTodo = {
             id: Date.now().toString(),
             todoItem,
             status,
+            todoDescription,
         } as Task;
 
-        axios.post("http://localhost:8081/task", newTodo);
+        const data = {
+            name: todoItem,
+            description: todoDescription,
+            status,
+            taskName: todoItem,
+        };
+
+        setUploadStatus("loading");
+
+        axios
+            .post("http://localhost:8081/task", data, {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            .then((resp) => {
+                console.log("response", resp.data);
+                setUploadStatus("success");
+                fetchTasks();
+            })
+            .catch((error) => {
+                console.log("error", error.response.data);
+                setUploadStatus("error");
+            });
 
         setTasks((prev) => [...prev, newTodo]);
     }
+
+    function fetchTasks() {
+        setFetchTaskStatus("loading");
+        axios
+            .get("http://localhost:8081/task")
+            .then((resp) => {
+                console.log("response", resp.data);
+
+                setFetchTaskStatus("success");
+
+                const newTasks = resp.data.map((item) => ({
+                    ...item,
+                    todoItem: item.taskName,
+                    todoDescription: item.description,
+                }));
+
+                setTasks((prev) => [...prev, ...newTasks]);
+            })
+            .catch((error) => {
+                console.log("error", error.response.data);
+
+                setFetchTaskStatus("error");
+            });
+    }
+
+    React.useEffect(() => {
+        fetchTasks();
+    }, []);
 
     function removeTodo(id: string) {
         setTasks((prev) => prev.filter((task) => task.id !== id));
     }
 
+    // if (uploadStatus === "loading" || fetchTaskStatus === "loading" || deleteStatus == "loading") {
+
+    if ([uploadStatus, fetchTaskStatus].includes("loading")) {
+        return <div>Loading...</div>;
+    }
+
+    if ([uploadStatus, fetchTaskStatus].includes("error")) {
+        return <div>Error</div>;
+    }
+
     return (
         <>
             <form onSubmit={addTodo}>
-                <input name="todoItem" type="text" />
+                <input name="todoItem" type="text" placeholder="task name" />
+                <input
+                    name="todoDescription"
+                    type="text"
+                    placeholder="description"
+                />
                 <select name="status">
                     <option value="TODO">Todo</option>
                     <option value="IN_PROGRESS">In Progress</option>
@@ -53,8 +129,9 @@ function App() {
             <ul>
                 {tasks.map((task, idx) => (
                     <li key={idx.toString()}>
-                        <span>{task.todoItem}</span>
-                        <span>{task.todoItem}</span>
+                        <span>{task.todoItem}</span> -
+                        <span>{task.todoDescription}</span> -
+                        <span>{task.status}</span>
                         <span>
                             <button
                                 type="button"
